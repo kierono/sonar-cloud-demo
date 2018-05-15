a13d24526ce7ca1349bbea3d66e2eba31215b238
Kieron O'Connor <kieron.oconnor@savilleassessment.com>
2018-05-15 09:17:56 +0100

    Add .gitignore and .gitattributes.

---
+++ Azure.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Azure.Web.Startup))]
+namespace Azure.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
